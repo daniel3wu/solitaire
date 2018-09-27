ee6f85747746e737b772a38ee4503c132a966b07
Danny Wu <daniel3wu@gmail.com>
2018-09-27 18:43:28 -0400

    Stock/waste functionality working

--- solitaire.py
+++ solitaire.py
@@ -1,13 +1,13 @@
 import random
 
 class Card():
-    card_to_name = {1:"Ace", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 6:"Six", 7:"Seven",
-                    8:"Eight", 9:"Nine", 10:"Ten", 11:"Jack", 12:"Queen", 13:"King"}
+    card_to_name = {1:"A", 2:"2", 3:"3", 4:"4", 5:"5", 6:"6", 7:"7",
+                    8:"8", 9:"9", 10:"10", 11:"J", 12:"Q", 13:"K"}
 
     def __init__(self, value, suit):
         self.name = self.card_to_name[value]
         self.suit = suit
-        self.title = "%s // %s" % (self.name, self.suit)
+        self.title = "%s%s" % (self.name, self.suit)
         self.value = value
 
     def isBelow(self, card):
@@ -27,7 +27,7 @@ class Card():
         return self.title
 
 class Deck():
-    unshuffled_deck = [Card(card, suit) for card in range(1, 14) for suit in ["c", "d", "h", "s"]]
+    unshuffled_deck = [Card(card, suit) for card in range(1, 14) for suit in ["club", "diam", "heart", "spade"]]
     def __init__(self, num_decks=1):
         self.deck = self.unshuffled_deck * num_decks
         random.shuffle(self.deck)
@@ -40,42 +40,41 @@ class Deck():
 
     def __str__(self):
     	return str(self.deck)
-
-# Pile of cards 
-class Pile(Deck):
-	known_cards = []
-
-	def __init__(self):
-		self.deck = []
-
-	def __init__(self, cards):
-		self.known_cards.append(cards.pop())
-		self.deck = cards
-
-	def attach_card(self, card):
-		pile_bottom = self.known_cards[-1]
-		if pile_bottom.canAttach(card):
-			self.known_cards.append(card)
-
-	def attach_cards(self, cards):
-		pile_bottom = self.known_cards[-1]
-		attachment = cards[0]
-		if pile_bottom.canAttach(attachment):
-			self.known_cards.extend(cards)
 		
 class Tableau():
 	# Seven piles
 	def __init__(self, deck):
 		self.hi = "hi"
 
-class Stock(Deck):
+	def attach_card(self, column, card):
+		return -1
+	def flip_card(self, column, card):
+		return -1
+
+class StockWaste():
 	waste = []
+
 	def __init__(self, cards):
 		self.deck = cards
 		random.shuffle(self.deck)
 
 	def flip_card(self):
-		waste.append(self.deck.pop())
+		self.waste.append(self.deck.pop())
+
+	def move_card(self, other_pile):
+		other_pile.attachCard(waste.pop())
+
+	def showWaste(self):
+		if len(self.waste) > 0:
+			return self.waste[-1]
+		else:
+			return "empty"
+
+	def showStock(self):
+		if len(self.deck) > 0:
+			return "X"
+		else:
+			return "empty"
 
 class Foundation():
 	foundation_piles = {"Clubs":[], "Hearts":[], "Spades":[], "Diamonds":[]}
@@ -87,7 +86,7 @@ class Foundation():
 		stack = foundation_piles[card.suit]
 		if len(stack) == 0 and card.value == 1:
 			foundation_piles[card.suit].append(card)
-		elif stack[len(stack)-1].isBelow(card):
+		elif stack[-1].isBelow(card):
 			foundation_piles[card.suit].append(card)
 		else:
 			print("Error! That card doesn't belong there.")
@@ -104,11 +103,11 @@ def printValidCommands():
 	print("\tq - quit")
 	print("\t*NOTE: Hearts/diamonds are red. Spades/clubs are black.")
 
-def printTable():
+def printTable(stock_waste):
 	""" Prints the current status of the table """
 	print("---------------------------------------------------------------------")
 	print("Waste \t Stock \t\t\t Foundation")
-	print("empty \t X \t\t 1 \t 2 \t 3 \t 4")
+	print(stock_waste.showWaste(), "\t", stock_waste.showStock(), "\t\t 1 \t 2 \t 3 \t 4")
 	print("\nTableau")
 	print("\t1\t2\t3\t4\t5\t6\t7\n")
 	print("---------------------------------------------------------------------")
@@ -116,12 +115,12 @@ def printTable():
 if __name__ == "__main__":
     d = Deck()
     f = Foundation()
-    s = Stock(d.deal_cards(14))
+    sw = StockWaste(d.deal_cards(14))
 
     print("\n-------------------------------------------------------------------")
     print("Welcome to Danny's Solitaire!\n")
     printValidCommands()
-    printTable()
+    printTable(sw)
     while True:
     	command = input("Enter a command (type 'h' for help): ")
     	command = command.lower()
@@ -131,3 +130,6 @@ if __name__ == "__main__":
     	elif command == "q":
     		print("Game exited.")
     		break
+    	elif command == "mv":
+    		sw.flip_card()
+    		printTable(sw)
