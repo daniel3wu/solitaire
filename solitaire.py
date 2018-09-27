229faf60bfa4b042bf05903a8f7b5dea5d15e4e6
Danny Wu <daniel3wu@gmail.com>
2018-09-27 18:24:22 -0400

    Created print statements for game flow

--- solitaire.py
+++ solitaire.py
@@ -19,7 +19,7 @@ class Card():
     	else:
     		return card.suit == "s" or cards.suit == "c"
 
-   	def canAttach(self, card):
+    def canAttach(self, card):
    		if card.isBelow(self) and card.oppositeSuit(self):
    			return True
 
@@ -33,8 +33,7 @@ class Deck():
         random.shuffle(self.deck)
 
     def flip_card(self):
-    	last_index = len(self.deck) - 1
-    	return self.deck.pop(last_index)
+    	return self.deck.pop()
 
     def deal_cards(self, num_cards):
     	return [self.deck.pop() for x in range(0, num_cards)]
@@ -54,12 +53,12 @@ class Pile(Deck):
 		self.deck = cards
 
 	def attach_card(self, card):
-		pile_bottom = self.known_cards[len(self.known_cards)-1]
+		pile_bottom = self.known_cards[-1]
 		if pile_bottom.canAttach(card):
 			self.known_cards.append(card)
 
 	def attach_cards(self, cards):
-		pile_bottom = self.known_cards[len(self.known_cards)-1]
+		pile_bottom = self.known_cards[-1]
 		attachment = cards[0]
 		if pile_bottom.canAttach(attachment):
 			self.known_cards.extend(cards)
@@ -67,6 +66,7 @@ class Pile(Deck):
 class Tableau():
 	# Seven piles
 	def __init__(self, deck):
+		self.hi = "hi"
 
 class Stock(Deck):
 	waste = []
@@ -75,11 +75,13 @@ class Stock(Deck):
 		random.shuffle(self.deck)
 
 	def flip_card(self):
-		last_index = len(self.deck) - 1
-		waste.append(self.deck.pop(last_index))
+		waste.append(self.deck.pop())
 
 class Foundation():
-	foundation_piles = {"Clubs":[], "Hearts":[], "Spades":[], "Diamonds:"[]}
+	foundation_piles = {"Clubs":[], "Hearts":[], "Spades":[], "Diamonds":[]}
+
+	def __init__(self):
+		self.completed = False
 
 	def attach_card(self, card):
 		stack = foundation_piles[card.suit]
@@ -90,7 +92,42 @@ class Foundation():
 		else:
 			print("Error! That card doesn't belong there.")
 
+def printValidCommands():
+	""" Provides the list of commands, for when users press 'h' """
+	print("Valid Commands: ")
+	print("\tmv - move card from Stock to Waste")
+	print("\twf #F - move card from Waste to Foundation")
+	print("\twt #T - move card from Waste to Tableau")
+	print("\ttf #T #F - move card from Tableau to Foundation")
+	print("\ttt #T1 #T2 - move card from one Tableau column to another")
+	print("\th - help")
+	print("\tq - quit")
+	print("\t*NOTE: Hearts/diamonds are red. Spades/clubs are black.")
 
+def printTable():
+	""" Prints the current status of the table """
+	print("---------------------------------------------------------------------")
+	print("Waste \t Stock \t\t\t Foundation")
+	print("empty \t X \t\t 1 \t 2 \t 3 \t 4")
+	print("\nTableau")
+	print("\t1\t2\t3\t4\t5\t6\t7\n")
+	print("---------------------------------------------------------------------")
 
 if __name__ == "__main__":
     d = Deck()
+    f = Foundation()
+    s = Stock(d.deal_cards(14))
+
+    print("\n-------------------------------------------------------------------")
+    print("Welcome to Danny's Solitaire!\n")
+    printValidCommands()
+    printTable()
+    while True:
+    	command = input("Enter a command (type 'h' for help): ")
+    	command = command.lower()
+    	command = command.replace(" ", "")
+    	if command == "h":
+    		printValidCommands()
+    	elif command == "q":
+    		print("Game exited.")
+    		break
